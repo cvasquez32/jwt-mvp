cbd26c802e487db6fc3d530c07633ff80e5c0a80
carlos vasquez <cvasquez0100@gmail.com>
2025-05-02 23:29:15 -0500

    init testing files for jwt token

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
 const app = express();
-const port = 3002;
 
-app.use(express.json());
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`Server Running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server Running at http://localhost:${process.env.PORT}`);
+});
+
+
+app.post("user/generateToken", (req, res) => {
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {
+    email: "test@gmail.com",
+  };
+
+  const token = jwt.sign(data, jwtSecretKey);
+
+  res.send(token);
+});
+
+app.get("user/validateToken", (req, res) => {
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+    const token = req.header(tokenHeaderKey);
+
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      return res.send("Successfully verified");
+    } else {
+      return res.status(401).send(error);
+    }
+  } catch (e) {
+    return res.status(401).send(e);
+  }
+});
+
+app.get("user/hello", (req, res) => {
+  res.send("Hello World");
 });
